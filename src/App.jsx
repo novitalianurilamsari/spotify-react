767ddf8e281d03de2e708a7a2cb905926b10a07e
Novita Lia Nurilam Sari <101916815+novitalianurilamsari@users.noreply.github.com>
2022-03-25 10:37:17 +0700

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import PlaylistContainer from './components/PlaylistContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Create Playlist<span>...</span></h1>
+      <h2 className="title2">now you can create your own playlist for free</h2>
+      <PlaylistContainer />
+      <div className="footer">
+        <h4 className="copyright">Homework5_KM-G2FE3045_Novita Lia NS</h4>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
