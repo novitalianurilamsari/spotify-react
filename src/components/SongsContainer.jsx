cbc9022e9b809656a41847598dc535eb40461bf1
Novita Lia Nurilam Sari <101916815+novitalianurilamsari@users.noreply.github.com>
2022-03-27 23:51:17 +0700

    Update and rename PlaylistContainer.jsx to SongsContainer.jsx

---
+++ src/components/SongsContainer.jsx
@@ -0,0 +1,26 @@
+import SongsItem from './SongsItem';
+import { SPOTIFY_DATA } from '../pages';
+
+function SongsContainer() {
+  /**
+   * @returns 
+   */
+  function renderSongsItems() {
+    return SPOTIFY_DATA.map((item) => {
+      const { id, album, name: judulLagu, artists } = item;
+      return (
+        <SongsItem
+          key={id}
+          image={album.images[0]?.url}
+          judulLagu={judulLagu}
+          albumName={album.name}
+          artists={artists}
+        />
+      );
+    });
+  }
+
+  return <div className="songs-container">{renderSongsItems()}</div>;
+}
+
+export default SongsContainer;
