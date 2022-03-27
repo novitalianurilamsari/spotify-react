68460324bb642e855850a954b3f10372dd827f03
Novita Lia Nurilam Sari <101916815+novitalianurilamsari@users.noreply.github.com>
2022-03-27 23:52:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import PlaylistContainer from './components/PlaylistContainer';
+import SongsContainer from './components/SongsContainer';
 
 function App() {
   return (
     <div className="app">
       <h1 className="title">Create Playlist<span>...</span></h1>
       <h2 className="title2">now you can create your own playlist for free</h2>
-      <PlaylistContainer />
+      <SongsContainer />
       <div className="footer">
-        <h4 className="copyright">Homework5_KM-G2FE3045_Novita Lia NS</h4>
+        <h4 className="copyright">Homework6_KM-G2FE3045_Novita Lia NS</h4>
       </div>
     </div>
     
